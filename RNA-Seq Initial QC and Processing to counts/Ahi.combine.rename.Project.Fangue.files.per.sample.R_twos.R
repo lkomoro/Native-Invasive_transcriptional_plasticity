cbe952559d2095d06a68ac53ca098e3294b8b7a6
lkomoro <lisa.komoroske@gmail.com>
2020-10-07 08:28:46 -0400

    subfolder upload

---
+++ RNA-Seq Initial QC and Processing to counts/Ahi.combine.rename.Project.Fangue.files.per.sample.R_twos.R
@@ -0,0 +1,13 @@
+rm(list = ls())
+#setwd('~/Project_Fangue_all_R2_files')#can't change working directory through script when 
+#workin on ahi-do command line so are where you want to be, then run script
+#getwd()
+file.list <- list.files(pattern = "R2")
+print(file.list)
+
+for (i in file.list) {
+  new.name <- paste(strsplit(i, c("-", "."))[[1]][2], ".fastq", sep = "")
+  t <- scan(i, what = 'character',sep="\r",quote="")
+  write(t, file = new.name, append=TRUE)
+  unlink(i) #include this if want it to delete original files
+}
